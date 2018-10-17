38343c8c7b5a35612ef9743ee422f7b29e15c3b4
bjorn andersson <bjorn@membrain.com>
2018-10-17 21:29:27 +0200

    New design update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,17 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Freelance Design Agency</h1>
-    <p>Design, prototypes, illustrations, motion graphics and much more</p>
-    <Link to="/page-2/">Watch Supercool Videos</Link>
+    <div  className="LayOut">
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Gladstone <br />Design</h1>
+          <p>Design, prototypes, illustrations, motion graphics and a whole bunch of
+          more stuff that'l knock you iff your feets so hard you wont get what hit ya.</p>
+          <Link to="/page-2/">Watch Supercool Videos</Link>
+        </div>
+      </div>
+    </div>
+
   </Layout>
 )
 
